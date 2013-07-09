1f655364d36660b41f4ba5094806c882292ad8b6
Julien HOMMET <hommet.julien@etu.unicaen.fr>
2013-07-09 21:02:21 +0200

    Ajout fichiers partie ADMIN

---
+++ admin/deltechnicien.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+include('../admin/id_bdd.php');
+
+$id				= $_GET["id"]; // Récupération du code news
+
+// La requête SQL se base sur l'ID sélectionné grâce au bouton à côté de la ligne concernée
+$sql 	= mysql_query ( " DELETE FROM ttechniciens WHERE id='$id'; " )  or  die ( mysql_error() ) ; // Exécution de la requête - Si pb, affichage erreur.
+
+Header("Location: techniciens.php"); // Redirection sur la page techniciens.php pour faire une suppression "transparente" pour l'utilisateur.
+?>
---
+++ admin/typemateriel.php
@@ -0,0 +1,40 @@
+﻿<?php include("id_bdd.php"); ?>
+
+<table border="1" rules="all">
+<tr> <th> MATERIEL </th> </tr>
+<?php
+	if (empty($_POST))
+	{ echo "<p>Pas de nouveau matériel ajouté</p><br /><hr /><br />"; }
+
+	else
+	{
+	// Récupération du nom
+		$materiel = $_POST["materiel"];
+
+	// Insertion des données dans la BDD
+		$add = mysql_query ( "INSERT INTO ttypemateriel VALUES ('','$materiel');" )  or  die( mysql_error() ) ;
+		echo "<center><h3>Matériel ' ".$materiel." ' ajouté avec succès !!</h3></center><br /><hr /><br />";
+	}	
+				
+// AFFICHAGE DES TYPES D'INTERVENTION
+	$req = mysql_query ( "SELECT * FROM ttypemateriel;" )  or  die( mysql_error() ) ;
+					  
+	while ( $ligne = mysql_fetch_array($req) )
+	{					  
+		echo "<tr>" ;
+		echo "<td>" . $ligne['materiel'] . "</td>" ;
+		echo "<td align='center'><form action='deltypemateriel.php?id=".$ligne["id"]."' method='POST'> <input type='hidden' name='id' value='".$ligne["id"]."'> <input type='submit' class='button blue morph' value='Suppression' onclick=\"javascript:return(confirm('Confirmer la suppression ?'))\";> </form></td>" ;
+		echo "</tr>" ;
+	}
+?>
+</table>
+<br /><hr /><br />
+<center>
+<h2>Ajout d'un matériel</h2>
+<form action="#" method="POST">
+	<p>
+	Nouveau matériel <input type="text" name="materiel" required><br />
+	<input type="submit" value="Ajouter">
+	</p>
+</form>
+</center>
