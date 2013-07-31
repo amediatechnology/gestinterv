a27cc861a95c5fc0122e064870b3c660af30deaf
Julien HOMMET <julien@idealogeek.fr>
2013-07-31 10:50:37 +0200

    Remplacement des "if" par des switchs

--- index.php
+++ index.php
@@ -32,25 +32,38 @@ if (empty($_GET["p"])) { header("Location: index.php?p=index"); }
 <?php
 // TITRES
 
-// Site
-if (($_GET['p'])=="index") { echo "<h1>Accueil - MIS Informatique - News</h1>"; }
-else if (($_GET['p'])=="recherche") { echo "<h1>Recherche d'un client</h1>"; }
-
-// Pré-interventions
-else if (($_GET['p'])=="ajoutpreinterv") { echo "<h1>Ajout d'une pré-intervention</h1>"; }
-
-// Interventions
-else if (($_GET['p'])=="showinterv") { echo "<h1>Affichage des interventions</h1>"; }
-else if (($_GET['p'])=="interv-preinterv") { echo "<h1>Transformation d'une <em>pré-intervention</em> en <u>intervention</u></h1>"; }
-else if (($_GET['p'])=="modifinterv") { echo "<h1>Modification d'une intervention</h1>"; }
-
-// Clients
-else if (($_GET['p'])=="clients") { echo "<h1>Liste des clients</h1>"; }
-else if (($_GET['p'])=="ficheclient") { echo "<h1>Fiche client</h1>"; }
-
-// Administration
-else if (($_GET['p'])=="administration") { echo "<h1>Administration générale du site</h1>"; }
-
+switch ($_GET['p']) {
+	case 'index':
+		echo "<h1>Accueil - MIS Informatique - News</h1>";
+	break;
+	case 'recherche':
+		echo "<h1>Recherche d'un client</h1>";
+	break;
+	case 'ajoutpreinterv':
+		echo "<h1>Recherche d'un client</h1>";
+	break;
+	case 'showinterv':
+		echo "<h1>Affichage des interventions</h1>";
+	break;
+	case 'interv-preinterv':
+		echo "<h1>Transformation d'une <em>pré-intervention</em> en <u>intervention</u></h1>";
+	break;
+	case 'modifinterv':
+		echo "<h1>Modification d'une intervention</h1>";
+	break;
+	case 'clients':
+		echo "<h1>Liste des clients</h1>";
+	break;
+	case 'ficheclient':
+		echo "<h1>Fiche client</h1>";
+	break;
+	case 'facturation':
+		echo "<h1>Création d'une facture client</h1>";
+	break;
+	case 'administration':
+		echo "<h1>Administration générale du site</h1>";
+	break;
+	}
 ?>
 
 <ul>
@@ -58,6 +71,7 @@ else if (($_GET['p'])=="administration") { echo "<h1>Administration générale d
 	<li><a href="index.php?p=ajoutpreinterv">Ajout d'une pré-intervention</a></li>
 	<li><a href="index.php?p=showinterv">Affichage des interventions</a></li>
 	<li><a href="index.php?p=clients">Liste des clients</a></li>
+	<li><a href="index.php?p=facturation">Facturation</a></li>
 	<li><a href="index.php?p=administration">./ Administration \.</a></li>
 </ul>
 <hr />
@@ -66,26 +80,38 @@ else if (($_GET['p'])=="administration") { echo "<h1>Administration générale d
 
 // CONTENU
 
-// Page d'accueil
-if (($_GET['p'])=="index")
-{ include "news/news.php"; }
-
-else if (($_GET['p'])=="recherche") { include('recherche.php'); }
-
-// Pré-interventions
-else if (($_GET['p'])=="ajoutpreinterv") { include('preintervention/index.php'); }
-
-// Interventions
-else if (($_GET['p'])=="showinterv") { include('intervention/affichageinterv.php'); }
-else if (($_GET['p'])=="interv-preinterv") { include('intervention/transfo-preinterv-interv.php'); }
-else if (($_GET['p'])=="modifinterv") { include('intervention/modif-intervention.php'); }
-
-// Clients
-else if (($_GET['p'])=="clients") { include('clients/affichageclients.php'); }
-else if (($_GET['p'])=="ficheclient") { include('clients/ficheclient.php'); }
-
-// Administration
-else if (($_GET['p'])=="administration") { include('admin/index.php'); }
+switch ($_GET['p']) {
+	case 'index':
+		include "news/news.php";
+	break;
+	case 'recherche':
+		include('recherche.php');
+	break;
+	case 'ajoutpreinterv':
+		include('preintervention/index.php');
+	break;
+	case 'showinterv':
+		include('intervention/affichageinterv.php');
+	break;
+	case 'interv-preinterv':
+		include('intervention/transfo-preinterv-interv.php');
+	break;
+	case 'modifinterv':
+		include('intervention/modif-intervention.php');
+	break;
+	case 'clients':
+		include('clients/affichageclients.php');
+	break;
+	case 'ficheclient':
+		include('clients/ficheclient.php');
+	break;
+	case 'facturation':
+		include('facturation/index.php');
+	break;
+	case 'administration':
+		include('admin/index.php');
+	break;
+	}
 ?>
 
 </body>
